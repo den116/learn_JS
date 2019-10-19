b94b9f7392525db5f91454e85840c30f72fae0a4
Денис Евдокимов <denisevdokimov@MacBook-Pro-Denis.local>
2019-10-19 17:27:25 +0300

    Основное задание

--- leason_1/script/script.js
+++ leason_1/script/script.js
@@ -1,10 +1,21 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
+let money = (50000), 
+    income = ('такси'), 
+    addExpenses = ('Гараж, Интернет, Кот'), 
+    deposit = (true), 
+    mission = (1000000), 
+    period = (12),
+    budgetDay = (money / 30);
 
+console.log('Тип данных "money":' + typeof money);
+console.log('Тип данных "income":' + typeof income);
+console.log('Тип данных "deposit":' + typeof deposit);
+console.log('Длина строки "income":' + income.length + ' символов');
+console.log('Период ' + period + ' месяцев' );
+console.log('Цель заработать ' + mission + ' рублей');
 
-alert ("hello World");    
-console.log(123);
+console.log(addExpenses.split(', '));
+console.log(addExpenses.toLowerCase());
+
+console.log('Дневной бюджет:' + budgetDay);
+console.log('Остаток от деления:' + (money % 30));
+ 
