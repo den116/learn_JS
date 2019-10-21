9de33e5d9096d4775420d473c758b035b013e2bc
Денис Евдокимов <denisevdokimov@MacBook-Pro-Denis.local>
2019-10-21 18:03:55 +0300

    Третий урок (обязательное задание)

--- leason_1/script/script.js
+++ leason_1/script/script.js
@@ -1,22 +1,43 @@
-let money = 50000, 
+let money = +prompt('Ваш месячный доход?',''),
     income = 'такси', 
-    addExpenses = 'Гараж, Интернет, Кот', 
-    deposit = true, 
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'), 
+    deposit = confirm('Есть ли у вас депозит в банке?'), 
     mission = 1000000, 
     period = 12,
-    budgetDay = money / 30;
+    monthlyExpenses01 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+    cash01 = +prompt('Во сколько это обойдется?'),
+    monthlyExpenses02 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+    cash02 = +prompt('Во сколько это обойдется?'),
+    budgetMonth = money - (cash01 + cash02),
+    budgetDay = +Math.floor(budgetMonth / 30);
 
-console.log('Тип данных "money":' + typeof money);
-console.log('Тип данных "income":' + typeof income);
-console.log('Тип данных "deposit":' + typeof deposit);
-console.log('Длина строки "income":' + income.length + ' символов');
-console.log('Период ' + period + ' месяцев' );
-console.log('Цель заработать ' + mission + ' рублей');
+// console.log('Тип данных "money":' + typeof money);
+// console.log('Тип данных "income":' + typeof income);
+// console.log('Тип данных "deposit":' + typeof deposit);
+// console.log('Длина строки "income":' + income.length + ' символов');
+// console.log('Период ' + period + ' месяцев' );
+// console.log('Цель заработать ' + mission + ' рублей');
 
 
 console.log(addExpenses.toLowerCase().split(', '));
 
-
+console.log('Тип данных "money":' + typeof money);
+console.log('Тип данных "income":' + typeof income);
+console.log('Тип данных "deposit":' + typeof deposit);
+console.log('Доход за месяц: ' + budgetMonth);
+console.log('Цель будет достигнута через: ' + Math.ceil(mission / budgetMonth) + ' месяцев');
 console.log('Дневной бюджет:' + budgetDay);
-console.log('Остаток от деления:' + (money % 30));
+
+if (budgetDay >= 800) {
+    console.log('Высокий уровень дохода');
+} else if (budgetDay > 300 && budgetDay < 800) {
+    console.log('Средний уровень дохода');
+} else if (budgetDay >= 0 && budgetDay < 300) {
+    console.log('Низкий уровень дохода');
+} else if (budgetDay < 0) {
+    console.log('Что то пошло не так');
+}
+
+
+// console.log('Остаток от деления:' + (money % 30));
  
