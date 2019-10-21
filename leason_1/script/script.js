e58a4df89b456d38e080d845aea0c77a38f5702d
Денис Евдокимов <denisevdokimov@MacBook-Pro-Denis.local>
2019-10-22 01:11:21 +0300

    правки третьекго основного задания

--- leason_1/script/script.js
+++ leason_1/script/script.js
@@ -1,14 +1,27 @@
-let money = +prompt('Ваш месячный доход?',''),
-    income = 'такси', 
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'), 
+let money;
+do {
+    money = +prompt('Ваш месячный доход?','30000');
+} while (isNaN(parseFloat(money)) && !isFinite(money));
+    
+let income = 'такси', 
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую','машина, дача, телефон'), 
     deposit = confirm('Есть ли у вас депозит в банке?'), 
     mission = 1000000, 
     period = 12,
-    monthlyExpenses01 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    cash01 = +prompt('Во сколько это обойдется?'),
-    monthlyExpenses02 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    cash02 = +prompt('Во сколько это обойдется?'),
-    budgetMonth = money - (cash01 + cash02),
+    monthlyExpenses01 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'интернет, кварт.плата, телефон'),
+    cash01;
+do {
+    cash01 = +prompt('Во сколько это обойдется?', '10000');
+} while (isNaN(parseFloat(cash01)) && !isFinite(cash01));
+
+let monthlyExpenses02 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'интернет, кварт.плата, телефон'),
+    cash02;
+do {
+    cash02 = +prompt('Во сколько это обойдется?', '10000');
+} while (isNaN(parseFloat(cash02)) && !isFinite(cash02));
+
+
+let budgetMonth = money - (cash01 + cash02),
     budgetDay = +Math.floor(budgetMonth / 30);
 
 // console.log('Тип данных "money":' + typeof money);
@@ -17,6 +30,16 @@ let money = +prompt('Ваш месячный доход?',''),
 // console.log('Длина строки "income":' + income.length + ' символов');
 // console.log('Период ' + period + ' месяцев' );
 // console.log('Цель заработать ' + mission + ' рублей');
+
+// !isNaN(parseFloat(n)) && isFinite(n);
+
+
+
+// if (!isNaN(parseFloat(money)) && isFinite(money)) {
+//     accessAllowed = true;
+//   } else {
+//     accessAllowed = false;
+//   }
 
 
 console.log(addExpenses.toLowerCase().split(', '));
@@ -30,7 +53,7 @@ console.log('Дневной бюджет:' + budgetDay);
 
 if (budgetDay >= 800) {
     console.log('Высокий уровень дохода');
-} else if (budgetDay > 300 && budgetDay < 800) {
+} else if (budgetDay >= 300 && budgetDay < 800) {
     console.log('Средний уровень дохода');
 } else if (budgetDay >= 0 && budgetDay < 300) {
     console.log('Низкий уровень дохода');
