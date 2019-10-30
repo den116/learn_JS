1cfecde1dfc38470a5d059c2697abf9f525d3062
Денис Евдокимов <denisevdokimov@MacBook-Pro-Denis.local>
2019-10-30 14:40:36 +0300

    Урок 7 (второе задание)

---
+++ budget_upp/script.js
@@ -0,0 +1,30 @@
+'Use strict';
+
+let startBtn = document.querySelector('#start'),
+    icomeBtn = document.querySelector('button'),
+    expensesBtn = document.querySelectorAll('button')[1],
+    checkBox = document.querySelector('#deposit-check'),
+    inputsIncome = document.querySelectorAll('.additional_income-item'),
+
+    budgetMonthValue = document.querySelector('.budget_month-value'),
+    budgetDayValue = document.querySelector('.budget_day-value'),
+    expensesMonthValue = document.querySelector('.expenses_month-value'),
+    additionalIncomeValue = document.querySelector('.additional_income-value'),
+    additionalExpensesValue = document.querySelector('.additional_expenses-value'),
+    incomePeriodValue = document.querySelector('.income_period-value'),
+    targetMonthValue = document.querySelector('.target_month-value'), 
+    
+    salaryAmount = document.querySelector('.salary-amount'),
+    incomeTitle = document.querySelector('.income-title'),
+    incomeAmount = document.querySelector('.income-amount'),
+    expensesTitle = document.querySelector('.expenses-title'),
+    expensesAmount = document.querySelector('.expenses-amount'),
+    additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+    targetAmount = document.querySelector('.target-amount'),
+    periodSelect = document.querySelector('.period-select');
+
+
+
+
+    
+
